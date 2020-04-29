ecd3607af7502303a78c494e84bf950e7217ae21
Abhigyan Raha <abhigyanraha76@gmail.com>
2020-04-29 12:15:16 +0530

    Ap29 2020 12:15

--- src/routers/AudioRouter.js
+++ src/routers/AudioRouter.js
@@ -16,6 +16,8 @@ router.post('/audio/new', authentication, async function(request, response){
     }
 })
 
+//search for audio information
+// {baseURL}/audio/search?trackname={trackname}&trackartist={trackartists}
 router.get('/audio/search', async function(request, response){
     const trackname = request.query.trackname;
     const trackartist = request.query.trackartist;
@@ -26,12 +28,21 @@ router.get('/audio/search', async function(request, response){
         console.log(e);
         response.status(400).send(e);
     }
-
 })
 
 //remove audio if it is user's audio
-router.delete('/audio/remove', authentication, async function(request, response){
-    
+router.delete('/audio/remove/:id', authentication, async function(request, response){
+    const audioID = request.params.id;
+    try{
+        const audio = await Audio.findOne({uploaded_by: request.user._id, _id: audioID})
+        if(!audio){
+            throw new Error('Audio not found');
+        }
+        await audio.remove();
+        response.send('removed')
+    }catch(e){
+        response.status(404).send(e);
+    }
 })
 
 //specialized audio removal route
@@ -40,13 +51,64 @@ router.delete('/audio/delete', authentication, async function(request,response){
 })
 
 //get audio
+//attach pagination to this!!!!!TODO!!!!!
 router.get('/audio/all', authentication, async function(request, response){
-
+    try{
+        const audios = await Audio.find({});
+        response.send(audios);
+    }catch(e){
+        response.status(404).send(e);
+    }
 })
 
 //get audio uploaded by user
 router.get('/audio/me', authentication, async function(request, response){
+    try{
+        const audios = await Audio.find({uploaded_by: request.user._id})
+        response.send(audios);
+    }catch(e){
+        response.status(404).send(e);
+    }
+})
 
+// /audio/find?t={title}
+router.get('/audio/find', authentication, async function(request, response){
+    try{
+        const audio = await Audio.findOne({title: request.query.t})
+        response.send(audio);
+    }catch(e){
+        response.status(404).send(e)
+    }
+})
+
+// /audio/find?id={id}
+router.get('/audio/findid', authentication, async function(request, response){
+    try{
+        const audio = await Audio.findOne({_id: request.query.id})
+        response.send(audio);
+    }catch(e){
+        response.status(404).send(e)
+    }
+})
+
+// /audio/find?artist={artist}
+router.get('/audio/findArtist', authentication, async function(request, response){
+    try{
+        const audio = await Audio.find({artist: request.query.artist})
+        response.send(audio);
+    }catch(e){
+        response.status(404).send(e)
+    }
+})
+
+// /audio/find?album={album}
+router.get('/audio/findAlbum', authentication, async function(request, response){
+    try{
+        const audio = await Audio.find({album: request.query.album})
+        response.send(audio);
+    }catch(e){
+        response.status(404).send(e)
+    }
 })
 
 module.exports = router;
